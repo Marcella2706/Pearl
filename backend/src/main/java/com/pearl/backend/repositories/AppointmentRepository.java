984c8d004ac83644b10f180710f5683d25640cd9
Marcella_2706 <saumyasood6@gmail.com>
2025-11-08 16:43:06 +0530

    new:fix

--- backend/src/main/java/com/pearl/backend/repositories/AppointmentRepository.java
+++ backend/src/main/java/com/pearl/backend/repositories/AppointmentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
     List<Appointment> findByDoctor(Users doctor);
     List<Appointment> findByPatient(Users patient);
     Optional<Appointment> findById(UUID id);
